601ab818b446d165b263c3405bfa5f0cb7c67438
sergey.zinovev1994 <sergey.zinovev1994@yandex.ru>
2024-11-07 13:50:22 +0300

    update bin/gendiff.js ('add help')

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,1 +1,12 @@
 #!/usr/bin/env node
+
+import { program } from 'commander';
+
+program
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.0.1')
+  .argument('<filePath1>', 'путь к 1му файлу')
+  .argument('<filePath2>', 'путь к 2му файлу');
+
+program.parse();
