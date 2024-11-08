02cf1f5e3f2b7ff4351dd019bf7b0b601afe1d6f
sergey.zinovev1994 <sergey.zinovev1994@yandex.ru>
2024-11-08 13:24:03 +0300

    update src/gendiff.js

--- src/gendiff.js
+++ src/gendiff.js
@@ -1,2 +1,10 @@
+import { readFileSync } from 'node:fs'
 
-
+export default (path1, path2) => {
+  const file1 = readFileSync(path1, 'utf8');
+  const file2 = readFileSync(path2, 'utf8');
+  const parseFile1 = JSON.parse(file1);
+  const parseFile2 = JSON.parse(file2);
+  console.log(parseFile1);
+  console.log(parseFile2);
+};
