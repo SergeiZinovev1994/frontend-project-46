d853992b8a4b9ef57b815c0111cec53ebb0815cb
sergey.zinovev1994 <sergey.zinovev1994@yandex.ru>
2024-11-08 21:18:58 +0300

    update src/gendiff.js

--- src/gendiff.js
+++ src/gendiff.js
@@ -1,10 +1,26 @@
 import { readFileSync } from 'node:fs'
 
+const parseFile = (path) => JSON.parse(readFileSync(path, 'utf8'));
+
 export default (path1, path2) => {
-  const file1 = readFileSync(path1, 'utf8');
-  const file2 = readFileSync(path2, 'utf8');
-  const parseFile1 = JSON.parse(file1);
-  const parseFile2 = JSON.parse(file2);
-  console.log(parseFile1);
-  console.log(parseFile2);
+  const file1 = parseFile(path1);
+  const file2 = parseFile(path2);
+  const keys = Object.keys( { ...file1, ...file2 }).sort();
+  const result = keys.reduce((acc, key) => {
+    if (Object.hasOwn(file1, key) && Object.hasOwn(file2, key)) {
+      if (file1[key] === file2[key]) {
+        acc[`  ${key}`] = file1[key];
+      } else {
+      acc[`- ${key}`] = file1[key];
+      acc[`+ ${key}`] = file2[key];
+      }
+    }
+    else if (Object.hasOwn(file1, key) && !Object.hasOwn(file2, key)) {
+      acc[`- ${key}`] = file1[key];
+    } else {
+      acc[`+ ${key}`] = file2[key];
+    }
+    return acc;
+  }, {});
+  return console.log(result);
 };
