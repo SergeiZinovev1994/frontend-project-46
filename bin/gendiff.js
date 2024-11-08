80de29302f8b1e1298152e114a7d8b71bd53532a
sergey.zinovev1994 <sergey.zinovev1994@yandex.ru>
2024-11-08 13:22:45 +0300

    add .action with a call to the gendiff function

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 import { program } from 'commander';
+import gendiff from '../src/gendiff.js';
 
 program
   .name('gendiff')
@@ -8,6 +9,8 @@ program
   .version('0.0.1')
   .argument('<filePath1>', 'путь к 1му файлу')
   .argument('<filePath2>', 'путь к 2му файлу')
-  .option('-f, --format [type]', 'output format');
+  .option('-f, --format [type]', 'output format')
+  .action((filePath1, filePath2) => {
+    gendiff(filePath1, filePath2)});
 
 program.parse();
