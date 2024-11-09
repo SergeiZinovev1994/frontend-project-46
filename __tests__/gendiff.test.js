2a06ad9264206199388fc7a4d44294f9265ffce1
sergey.zinovev1994 <sergey.zinovev1994@yandex.ru>
2024-11-09 19:26:53 +0300

    update __tests__/gendiff.test.js

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,2 +1,10 @@
-import gendiff from "../src/gendiff.js";
+import { gendiff, parseFile } from "../src/gendiff.js";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+test('simpleTest', () => {
+  expect(gendiff(`${__dirname}/../__fixtures__/simpleObject1.json`, `${__dirname}/../__fixtures__/simpleObject2.json`)).toEqual(parseFile(`${__dirname}/../__fixtures__/simpleResult.json`));
+});
