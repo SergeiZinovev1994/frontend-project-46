8a2899f5fa51dd155ca97766a31bc59e32432234
sergey.zinovev1994 <sergey.zinovev1994@yandex.ru>
2024-11-15 16:25:55 +0300

    moved the parse files to the module

--- src/gendiff.js
+++ src/gendiff.js
@@ -1,12 +1,10 @@
-import { readFileSync } from 'node:fs';
 import _ from 'lodash';
-
-export const parseFile = (path) => JSON.parse(readFileSync(path, 'utf8'));
+import parseFile from './parsers.js';
 
 export const gendiff = (path1, path2) => {
   const file1 = parseFile(path1);
   const file2 = parseFile(path2);
-  const keys = _.sortedUniq(Object.keys( { ...file1, ...file2 }));
+  const keys = _.sortedUniq(Object.keys({ ...file1, ...file2 }));
   const result = keys.reduce((acc, key) => {
     if (Object.hasOwn(file1, key) && Object.hasOwn(file2, key)) {
       if (file1[key] === file2[key]) {
