97fbe5cf1d7238d0da48000ebd4cb50d27248493
sergey.zinovev1994 <sergey.zinovev1994@yandex.ru>
2024-11-15 16:24:09 +0300

    update __tests__/gendiff.test.js

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,13 +1,40 @@
 /* eslint-env jest */
 
-import { gendiff, parseFile } from "../src/gendiff.js";
+import { gendiff } from '../src/gendiff.js';
+import parseFile from '../src/parsers.js'
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
-import { test, expect } from '@jest/globals';
+import { test, expect, describe } from '@jest/globals';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-test('simpleTest', () => {
-  expect(gendiff(`${__dirname}/../__fixtures__/simpleObject1.json`, `${__dirname}/../__fixtures__/simpleObject2.json`)).toEqual(parseFile(`${__dirname}/../__fixtures__/simpleResult.json`));
+const commonPath = (fileName) => `${__dirname}/../__fixtures__/${fileName}`;
+
+describe ('gendiffTests', () => {
+  test('jsonWithJson', () => {
+  expect(gendiff(commonPath('simpleObject1.json'), commonPath('simpleObject2.json')))
+  .toEqual(parseFile(commonPath('simpleResult.json')));
 });
+  test('ymlWithYaml', () => {
+    expect(gendiff(commonPath('file1.yml'), commonPath('file2.yaml')))
+    .toEqual(parseFile(commonPath('resultWithYml.json')));
+  });
+});
+
+test('parseFileTest', () => {
+  expect(parseFile(commonPath('file1.json')))
+  .toEqual({
+  "host": "hexlet.io",
+  "timeout": 50,
+  "proxy": "123.234.53.22",
+  "follow": false
+});
+  expect(parseFile(commonPath('file1.yml')))
+  .toEqual({
+  "host": "hexlet.io",
+  "timeout": 50,
+  "proxy": "123.234.53.22",
+  "follow": false
+});
+})
