d025dd30e58ed42f28fa3a866934a955c6906037
sergey.zinovev1994 <sergey.zinovev1994@yandex.ru>
2025-03-11 16:11:53 +0300

    update bin/gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,6 +2,7 @@
 
 import { program } from 'commander';
 import { gendiff } from '../src/gendiff.js';
+import { formatter } from '../src/formatter.js';
 
 program
   .name('gendiff')
@@ -10,7 +11,8 @@ program
   .arguments('<filepath1> <filepath2>')
   .option('-f, --format [type]', 'output format')
   .action((filepath1, filepath2) => {
-    console.log(gendiff(filepath1, filepath2));
+    const a = gendiff(filepath1, filepath2);
+    console.log(formatter(a));
   });
 
 program.parse(process.argv);
