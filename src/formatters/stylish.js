1359336d32316268a53855ef2c29da5e570dc15f
sergey.zinovev1994 <sergey.zinovev1994@yandex.ru>
2025-03-13 14:12:51 +0300

    change path from "src/stylish.js" to "src/formatters/stylish.js"

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,17 +1,17 @@
-export const formatter = (arr, depth = 1) => {
+export const stylish = (data, depth = 1) => {
   const spacesPerDepth = 4;
   const baseOffsetIndex = 2;
   const countTabs = depth * spacesPerDepth - baseOffsetIndex;
-  return arr.reduce((acc , { key, body, type, sign }) => {
+  return data.reduce((acc , { key, body, type, sign }) => {
   switch(type) {
-    case 'onlyInContent1':
-    case 'onlyInContent2':
+    case 'added':
+    case 'removed':
       acc[`${' '.repeat(countTabs)}${sign}${key}`] = body;
       return acc;
     case 'equal':
       acc[`${' '.repeat(countTabs)}${sign}${key}`] = body;
       return acc;
-    case '0 or 1 objects': {
+    case 'updated': {
         const { signCont1, signCont2 } = sign;
         const { content1, content2 } = body;
       acc[`${' '.repeat(countTabs)}${signCont1}${key}`] = content1;
@@ -19,7 +19,7 @@ export const formatter = (arr, depth = 1) => {
       return acc;
     }
     case 'differentObjects': {
-      acc[`${' '.repeat(countTabs)}${sign}${key}`] = formatter(body, depth + 1);
+      acc[`${' '.repeat(countTabs)}${sign}${key}`] = stylish(body, depth + 1);
       return acc;
     }
   }
