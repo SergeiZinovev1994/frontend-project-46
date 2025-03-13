a56f54192fa91715f00a89436a8c3e1fca5c20bb
sergey.zinovev1994 <sergey.zinovev1994@yandex.ru>
2025-03-13 21:42:09 +0300

    update bin/gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,17 +2,18 @@
 
 import { program } from 'commander';
 import { gendiff } from '../src/gendiff.js';
-import index from '../src/formatters/index.js';
+import start from '../src/formatters/index.js';
 
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1')
   .arguments('<filepath1> <filepath2>')
-  .option('-f, --format [type]', 'output format', 'stylish')
-  .action((filepath1, filepath2, type) => {
+  .option('-f, --format <type>', 'output format', 'stylish')
+  .action((filepath1, filepath2) => {
     const data = gendiff(filepath1, filepath2);
-    console.log(index(data, type.format));
+    const formatName = program.opts().format;
+    console.log(start(data, formatName));
   });
 
 program.parse(process.argv);
