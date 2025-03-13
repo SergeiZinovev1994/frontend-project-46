8d8a28e98c07ba6ee7799c0f6590528f41de69ff
sergey.zinovev1994 <sergey.zinovev1994@yandex.ru>
2025-03-13 14:14:41 +0300

    update src/gendiff.js

--- src/gendiff.js
+++ src/gendiff.js
@@ -11,13 +11,13 @@ export const gendiff = (path1, path2) => {
     const keys = _.uniq(Object.keys({ ...content1, ...content2 }));
     const result = keys.toSorted().map((key) => {
     if (!Object.hasOwn(content2, key)) {
-      return { key, body: content1[key], type: 'onlyInContent1', sign: '- '};
+      return { key, body: content1[key], type: 'removed', sign: '- '};
     } else if (!Object.hasOwn(content1, key)) {
-      return { key, body: content2[key], type: 'onlyInContent2', sign: '+ '};
+      return { key, body: content2[key], type: 'added', sign: '+ '};
     } else if (_.isEqual(content1[key], content2[key])) {
       return { key, body: content1[key], type: 'equal', sign: '  ' };
     } else if (!_.isPlainObject(content1[key]) || !_.isPlainObject(content2[key])) {
-      return { key, body: { content1: content1[key], content2: content2[key] }, type: '0 or 1 objects', sign: { signCont1: '- ', signCont2: '+ '} };
+      return { key, body: { content1: content1[key], content2: content2[key] }, type: 'updated', sign: { signCont1: '- ', signCont2: '+ '} };
     } else {
       return { key, body: core(content1[key], content2[key]), type: 'differentObjects', sign: '  '}
     }
