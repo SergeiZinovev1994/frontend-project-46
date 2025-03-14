f8d19ddb812d01160b0b6ac2bc9c10930429d696
sergey.zinovev1994 <sergey.zinovev1994@yandex.ru>
2025-03-14 11:32:36 +0300

    change src/helpers.js

--- src/helpers.js
+++ src/helpers.js
@@ -6,6 +6,6 @@ import parseFile from './parsers.js';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-export const getFixturesPath = (fileName) => join(__dirname, '..', '__fixtures__/', fileName);
+export const getFixturePath = (fileName) => join(__dirname, '..', '__fixtures__/', fileName);
 
 export const readContent = (fileName) => parseFile(resolve(process.cwd(), fileName));
