121a41ccb3647eeba562dd44ca932f628a4894dd
sergey.zinovev1994 <sergey.zinovev1994@yandex.ru>
2025-03-15 18:24:54 +0300

    change src/formatters/stylish.js

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,27 +1,58 @@
-export const stylish = (data, depth = 1) => {
-  const spacesPerDepth = 4;
-  const baseOffsetIndex = 2;
-  const countTabs = depth * spacesPerDepth - baseOffsetIndex;
-  return data.reduce((acc , { key, body, type, sign }) => {
-  switch(type) {
-    case 'added':
-    case 'removed':
-      acc[`${' '.repeat(countTabs)}${sign}${key}`] = body;
-      return acc;
-    case 'equal':
-      acc[`${' '.repeat(countTabs)}${sign}${key}`] = body;
-      return acc;
-    case 'updated': {
-        const { signCont1, signCont2 } = sign;
-        const { content1, content2 } = body;
-      acc[`${' '.repeat(countTabs)}${signCont1}${key}`] = content1;
-      acc[`${' '.repeat(countTabs)}${signCont2}${key}`] = content2;
-      return acc;
+import _ from 'lodash';
+
+const indentDepth = (depth, flag = true) => {
+    const spacesPerDepth = 4;
+    const baseIndex = 2;
+    return flag ? depth * spacesPerDepth - baseIndex : depth * spacesPerDepth;
+  };
+
+const getData = (content, ind) => {
+  const iter = (data, depth) => {
+    if (!_.isObject(data)) {
+      return data;
     }
-    case 'differentObjects': {
-      acc[`${' '.repeat(countTabs)}${sign}${key}`] = stylish(body, depth + 1);
-      return acc;
-    }
+    const countTabs = indentDepth(depth, false);
+    const keys = Object.keys(data);
+    const result = keys.map((key) => {
+      const formattedBody = _.isObject(data[key]) ?
+      `{\n${iter(data[key], depth + 1)}\n${' '.repeat(countTabs)}}` :
+      iter(data[key], depth);
+
+    return `${' '.repeat(countTabs)}${key}: ${formattedBody}`;
+  });
+    return `${result.join('\n')}`;
   }
-}, {});
+  return iter(content, ind);
+};
+
+export const stylish = (content) => {
+  const iter = (data, depth = 1) => {
+    const countTabs = indentDepth(depth);
+    const countIndent = indentDepth(depth, false);
+
+    const formatBody = (data) => (_.isObject(data) ?
+    `{\n${getData(data, depth + 1)}\n${' '.repeat(countIndent)}}`:
+    getData(data, depth + 1));
+
+    const formatString = (indent, sign, key, body) => `${' '.repeat(indent)}${sign}${key}: ${body}`;
+
+    return data.flatMap(({ key, body, type, sign }) => {
+      switch(type) {
+        case 'added':
+        case 'removed':
+        case 'equal': {
+          return `${formatString(countTabs, sign, key, formatBody(body))}`;
+        }
+        case 'updated': {
+          const { signCont1, signCont2 } = sign;
+          const { content1, content2 } = body;
+          return `${formatString(countTabs, signCont1, key, formatBody(content1))}\n${formatString(countTabs, signCont2, key, formatBody(content2))}`;
+        }
+        case 'differentObjects': {
+          return `${' '.repeat(countTabs)}${sign}${key}: {\n${iter(body, depth + 1).join('\n')}\n${' '.repeat(countIndent)}}`;
+        }
+      }
+    });
+  };
+  return `{\n${iter(content).join('\n')}\n}`;
 };
