a1f5a066d6c1640a2b34e5d119bfa9a7c7eae9b8
sergey.zinovev1994 <sergey.zinovev1994@yandex.ru>
2025-03-15 19:25:13 +0300

    change index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import { gendiff } from "./src/gendiff.js";
 import formatData from './src/formatters/index.js';
 
-
 export default (filePath1, filePath2 , formatName = 'stylish') => {
   const data = gendiff(filePath1, filePath2);
   return formatData(data, formatName);
