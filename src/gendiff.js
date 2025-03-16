441acbde7b8955d1549beb55b1ba09e7dad1b9f9
sergey.zinovev1994 <sergey.zinovev1994@yandex.ru>
2025-03-16 10:23:22 +0300

    change src/gendiff.js

--- src/gendiff.js
+++ src/gendiff.js
@@ -6,20 +6,21 @@ export const gendiff = (path1, path2) => {
   const file2 = readContent(path2);
   const core = (content1, content2) => {
     const keys = _.uniq(Object.keys({ ...content1, ...content2 }));
-    const result = keys.toSorted().map((key) => {
+    return keys.toSorted().map((key) => {
+    let node;
     if (!Object.hasOwn(content2, key)) {
-      return { key, body: content1[key], type: 'removed', sign: '- ' };
+      node = { key, body: content1[key], type: 'removed', sign: '- ' };
     } else if (!Object.hasOwn(content1, key)) {
-      return { key, body: content2[key], type: 'added', sign: '+ ' };
+      node = { key, body: content2[key], type: 'added', sign: '+ ' };
     } else if (_.isEqual(content1[key], content2[key])) {
-      return { key, body: content1[key], type: 'equal', sign: '  ' };
+      node = { key, body: content1[key], type: 'equal', sign: '  ' };
     } else if (!_.isPlainObject(content1[key]) || !_.isPlainObject(content2[key])) {
-      return { key, body: { content1: content1[key], content2: content2[key] }, type: 'updated', sign: { signCont1: '- ', signCont2: '+ ' } };
+      node = { key, body: { content1: content1[key], content2: content2[key] }, type: 'updated', sign: { signCont1: '- ', signCont2: '+ ' } };
     } else {
-      return { key, body: core(content1[key], content2[key]), type: 'differentObjects', sign: '  ' }
+      node = { key, body: core(content1[key], content2[key]), type: 'differentObjects', sign: '  ' }
     }
+    return node;
     });
-    return result;
   };
   return core(file1, file2);
 };
