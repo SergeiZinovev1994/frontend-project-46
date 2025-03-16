844ce82bb35487aa109110ce9a640989015f16b4
sergey.zinovev1994 <sergey.zinovev1994@yandex.ru>
2025-03-16 19:14:45 +0300

    fix eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -7,6 +7,13 @@ import { FlatCompat } from '@eslint/eslintrc';
 import pluginJs from '@eslint/js';
 
 // mimic CommonJS variables -- not needed if using CommonJS
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: pluginJs.configs.recommendedб
+});
+
 export default [
   { languageOptions: { globals: globals.node } },
   ...compat.extends('airbnb-base'),
