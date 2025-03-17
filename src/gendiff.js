e6a1edeefd4b1b082fbb2cf8e09b3e00662a7d7e
sergey.zinovev1994 <sergey.zinovev1994@yandex.ru>
2025-03-17 15:03:44 +0300

    fix eslint errors in src/gendiff.js

--- src/gendiff.js
+++ src/gendiff.js
@@ -1,33 +1,35 @@
 import _ from 'lodash';
 
+const createNode = (key, body, type, sign) => ({
+  key,
+  body,
+  type,
+  sign,
+});
+
 export default (file1, file2) => {
   const core = (content1, content2) => {
     const keys = _.uniq(Object.keys({ ...content1, ...content2 }));
     return keys.toSorted().map((key) => {
       if (!Object.hasOwn(content2, key)) {
-        return {
-          key, body: content1[key], type: 'removed', sign: '- ',
-        };
+        return createNode(key, content1[key], 'removed', '- ');
       }
       if (!Object.hasOwn(content1, key)) {
-        return {
-          key, body: content2[key], type: 'added', sign: '+ ',
-        };
+        return createNode(key, content2[key], 'added', '+ ');
       }
       if (_.isEqual(content1[key], content2[key])) {
-        return {
-          key, body: content1[key], type: 'equal', sign: '  ',
-        };
+        return createNode(key, content1[key], 'equal', '  ');
       }
       if (!_.isPlainObject(content1[key]) || !_.isPlainObject(content2[key])) {
-        return {
-          key, body: { content1: content1[key], content2: content2[key] }, type: 'updated', sign: { signCont1: '- ', signCont2: '+ ' },
-        };
+        return createNode(
+          key,
+          { content1: content1[key], content2: content2[key] },
+          'updated',
+          { signCont1: '- ', signCont2: '+ ' },
+        );
       }
       if ((_.isPlainObject(content1[key]) && _.isPlainObject(content2[key]))) {
-        return {
-          key, body: core(content1[key], content2[key]), type: 'differentObjects', sign: '  ',
-        };
+        return createNode(key, core(content1[key], content2[key]), 'differentObjects', '  ');
       }
       throw new Error(`${key} was not finded`);
     });
