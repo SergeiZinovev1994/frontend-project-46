c1eebfa1ec1ea4804c3b19d818271488be9b3dd9
sergey.zinovev1994 <sergey.zinovev1994@yandex.ru>
2025-03-19 13:10:40 +0300

    change src/gendiff.js

--- src/gendiff.js
+++ src/gendiff.js
@@ -5,29 +5,19 @@ export default (file1, file2) => {
     const keys = _.uniq(Object.keys({ ...content1, ...content2 }));
     return keys.toSorted().map((key) => {
       if (!Object.hasOwn(content2, key)) {
-        return {
-          key, body: content1[key], type: 'removed', sign: '- ',
-        };
+        return { key, body: content1[key], type: 'removed' };
       }
       if (!Object.hasOwn(content1, key)) {
-        return {
-          key, body: content2[key], type: 'added', sign: '+ ',
-        };
+        return { key, body: content2[key], type: 'added' };
       }
       if (_.isEqual(content1[key], content2[key])) {
-        return {
-          key, body: content1[key], type: 'equal', sign: '  ',
-        };
+        return { key, body: content1[key], type: 'equal' };
       }
       if (!_.isPlainObject(content1[key]) || !_.isPlainObject(content2[key])) {
-        return {
-          key, body: { content1: content1[key], content2: content2[key] }, type: 'updated', sign: { signCont1: '- ', signCont2: '+ ' },
-        };
+        return { key, body: { content1: content1[key], content2: content2[key] }, type: 'updated' };
       }
       if ((_.isPlainObject(content1[key]) && _.isPlainObject(content2[key]))) {
-        return {
-          key, body: core(content1[key], content2[key]), type: 'differentObjects', sign: '  ',
-        };
+        return { key, body: core(content1[key], content2[key]), type: 'differentObjects' };
       }
       throw new Error(`${key} was not finded`);
     });
