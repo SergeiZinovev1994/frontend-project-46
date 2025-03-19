69dc526fc8e9b84d75e2578f7ba47b41a0990b73
sergey.zinovev1994 <sergey.zinovev1994@yandex.ru>
2025-03-19 12:35:38 +0300

    change src/parsers.js

--- src/parsers.js
+++ src/parsers.js
@@ -3,12 +3,12 @@ import { extname } from 'path';
 import yaml from 'js-yaml';
 
 export default (pathFile) => {
-  const extension = extname(pathFile);
+  const extension = extname(pathFile).slice(1);
   switch (extension) {
-    case '.json':
+    case 'json':
       return JSON.parse(readFileSync(pathFile, 'utf8'));
-    case '.yml':
-    case '.yaml':
+    case 'yml':
+    case 'yaml':
       return yaml.load(readFileSync(pathFile, 'utf8'));
     default:
       throw new Error(`${extension} - unknown extension`);
