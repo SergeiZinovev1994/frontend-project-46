3f1d510b0734a96e4ad22b97594497c91253311d
sergey.zinovev1994 <sergey.zinovev1994@yandex.ru>
2025-03-19 12:36:00 +0300

    change src/gendiff.js

--- src/gendiff.js
+++ src/gendiff.js
@@ -1,35 +1,33 @@
 import _ from 'lodash';
 
-const createNode = (key, body, type, sign) => ({
-  key,
-  body,
-  type,
-  sign,
-});
-
 export default (file1, file2) => {
   const core = (content1, content2) => {
     const keys = _.uniq(Object.keys({ ...content1, ...content2 }));
     return keys.toSorted().map((key) => {
       if (!Object.hasOwn(content2, key)) {
-        return createNode(key, content1[key], 'removed', '- ');
+        return {
+          key, body: content1[key], type: 'removed', sign: '- ',
+        };
       }
       if (!Object.hasOwn(content1, key)) {
-        return createNode(key, content2[key], 'added', '+ ');
+        return {
+          key, body: content2[key], type: 'added', sign: '+ ',
+        };
       }
       if (_.isEqual(content1[key], content2[key])) {
-        return createNode(key, content1[key], 'equal', '  ');
+        return {
+          key, body: content1[key], type: 'equal', sign: '  ',
+        };
       }
       if (!_.isPlainObject(content1[key]) || !_.isPlainObject(content2[key])) {
-        return createNode(
-          key,
-          { content1: content1[key], content2: content2[key] },
-          'updated',
-          { signCont1: '- ', signCont2: '+ ' },
-        );
+        return {
+          key, body: { content1: content1[key], content2: content2[key] }, type: 'updated', sign: { signCont1: '- ', signCont2: '+ ' },
+        };
       }
       if ((_.isPlainObject(content1[key]) && _.isPlainObject(content2[key]))) {
-        return createNode(key, core(content1[key], content2[key]), 'differentObjects', '  ');
+        return {
+          key, body: core(content1[key], content2[key]), type: 'differentObjects', sign: '  ',
+        };
       }
       throw new Error(`${key} was not finded`);
     });
