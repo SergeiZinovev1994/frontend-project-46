f9f7c5e1ab5768b9ac2f586f69b0832519456e34
sergey.zinovev1994 <sergey.zinovev1994@yandex.ru>
2025-03-21 13:59:14 +0300

    moved the getFixturePath function to the tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -2,7 +2,13 @@
 import { test, expect, describe } from '@jest/globals';
 import fs from 'fs';
 import genDiff from '../index.js';
-import { getFixturePath } from '../src/helpers.js';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const getFixturePath = (fileName) => join(__dirname, '..', '__fixtures__/', fileName);
 
 describe('gendiff', () => {
   test.each([
--- src/helpers.js
+++ src/helpers.js
@@ -1,11 +1,7 @@
 import process from 'node:process';
 import { resolve, dirname, join } from 'path';
-import { fileURLToPath } from 'url';
 import parseFile from './parsers.js';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
-export const getFixturePath = (fileName) => join(__dirname, '..', '__fixtures__/', fileName);
 
 export const readContent = (fileName) => parseFile(resolve(process.cwd(), fileName));
