2bf9f3aa17522a488de0c32680e5ae5ff99b025e
sergey.zinovev1994 <sergey.zinovev1994@yandex.ru>
2025-03-21 20:20:11 +0300

    change src/helpers.js

--- src/helpers.js
+++ src/helpers.js
@@ -1,11 +1,11 @@
 import process from 'node:process';
 import { resolve, extname } from 'path';
-import parseFile from './parsers.js';
 import { readFileSync } from 'node:fs';
+import parseFile from './parsers.js';
 
 const getContentAndType = (path) => {
   const extension = extname(path).slice(1);
-  return {content: readFileSync(path, 'utf-8'), format: extension };
-}
+  return { content: readFileSync(path, 'utf-8'), format: extension };
+};
 
 export default (fileName) => parseFile(getContentAndType(resolve(process.cwd(), fileName)));
